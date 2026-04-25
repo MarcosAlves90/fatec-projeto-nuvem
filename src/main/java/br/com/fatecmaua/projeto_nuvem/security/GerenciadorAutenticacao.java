5baeefb38847c658a8665c3aaa7fb8501cbbf973
Marcos Alves <94864806+MarcosAlves90@users.noreply.github.com>
2026-04-25 12:11:11 -0300

    feat: add authentication manager and security configuration classes
    This commit introduces the GerenciadorAutenticacao class for managing authentication and a placeholder SegurancaConfig class for future security configurations.

--- src/main/java/br/com/fatecmaua/projeto_nuvem/security/GerenciadorAutenticacao.java
+++ src/main/java/br/com/fatecmaua/projeto_nuvem/security/GerenciadorAutenticacao.java
@@ -1,14 +1,16 @@
 package br.com.fatecmaua.projeto_nuvem.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
-public class GerenciadorAutenticacoes {
-
-    public AuthenticationManager autenticar(AuthenticationConfiguration config) throws Exception{
-        return config.getAuthenticationManager();
-    }
-
+public class GerenciadorAutenticacao {
+	
+	@Bean
+	public AuthenticationManager autenticar(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+ 
 }
---
+++ src/main/java/br/com/fatecmaua/projeto_nuvem/security/SegurancaConfig.java
@@ -0,0 +1,5 @@
+package br.com.fatecmaua.projeto_nuvem.security;
+
+public class SegurancaConfig {
+
+}
